ddf39770cd7485e246343699e78638f652c77397
Abhinav Bajaj <abhinav_bajaj2009@yahoo.com>
2018-04-23 16:05:20 -0700

    Check if key exists in map

--- src/main/java/com/satalyst/powerbi/operations/MapUtils.java
+++ src/main/java/com/satalyst/powerbi/operations/MapUtils.java
@@ -10,23 +10,33 @@ import java.util.UUID;
  */
 public class MapUtils {
 
-    public static String getString(Map map, String name) {
-        return (String) map.get(name);
-    }
+	public static String getString(Map map, String name) {
+		if (map.containsKey(name))
+			return (String) map.get(name);
+		return "";
+	}
 
-    public static List<Map> getList(Map map, String name) {
-        return (List<Map>) map.get(name);
-    }
+	public static List<Map> getList(Map map, String name) {
+		if (map.containsKey(name))
+			return (List<Map>) map.get(name);
+		return null;
+	}
 
-    public static UUID getUuid(Map map, String name) {
-        return UUID.fromString(getString(map, name));
-    }
+	public static UUID getUuid(Map map, String name) {
+		if (map.containsKey(name))
+			return UUID.fromString(getString(map, name));
+		return null;
+	}
 
-    public static List<Column> getColumns(Map map, String name) {
-        return (List<Column>) map.get(name);
-    }
+	public static List<Column> getColumns(Map map, String name) {
+		if (map.containsKey(name))
+			return (List<Column>) map.get(name);
+		return null;
+	}
 
-    public static Double getDouble(Map map, String name) {
-        return (Double) map.get(name);
-    }
+	public static Double getDouble(Map map, String name) {
+		if (map.containsKey(name))
+			return (Double) map.get(name);
+		return null;
+	}
 }
